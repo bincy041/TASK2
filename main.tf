9cf259283f8414176b4f4658d9adcc033de63cc8
bincy041 <82860563+bincy041@users.noreply.github.com>
2021-04-27 09:40:52 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,17 @@
+locals {
+  
+  environment = [for environment in var.environment : substr(environment, 0, 1)]
+  os          = [for os in var.os : substr(os, 0, 1)]
+  
+  
+  az_zone1        = element([for azs in var.azs : element(split("", element(split("-", azs), 0)), 0)], 0)
+  az_zone2        = [for azs in var.azs : element(split("-", azs), 1)]
+ 
+     
+ Example      = setproduct(["${var.region}"],["${local.az_zone1}"],["${local.az_zone2[1]}"],["${local.environment[0]}"],["${local.os[0]}"])
+ 
+ 
+ #Example      = setproduct(["${var.region}"],["${local.az_zone1}"],["${local.az_zone2}"],["${local.environment}"],["${local.os}"])
+ # num             = "${var.region}${local.az_zone1}${local.az_zone2[0]}${local.environment[*]}${local.os[*]}}"
+
+}
